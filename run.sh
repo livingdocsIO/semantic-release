b369cd4e1b5e67f772ee65ee328bcc41d71f9c72
Marc Bachmann <marc.brookman@gmail.com>
2019-11-30 04:48:31 +0100

    feat: Always execute set-ci using a postversion hook. We also overwrite the original hooks to prevent malicious code execution

--- run.sh
+++ run.sh
@@ -2,8 +2,19 @@
 
 git fetch --tags -q
 
+read -r -d '' PATCHPACKAGE << EOL
+const pkg = require("./package.json");
+const scripts = pkg.scripts || {};
+scripts.preversion = undefined;
+scripts.postversion = "/release/node_modules/.bin/set-ci";
+pkg.scripts = scripts;
+fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 2));
+EOL
+
+node -e "$PATCHPACKAGE"
+
 if [ "$DRY_RUN" = "true" ]; then
-  /release/node_modules/.bin/semantic-release --dry-run
+  exec /release/node_modules/.bin/semantic-release --dry-run
 else
-  /release/node_modules/.bin/semantic-release
+  exec /release/node_modules/.bin/semantic-release
 fi
