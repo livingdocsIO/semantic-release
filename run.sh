a4e1b1ca7d426778d1d6c994c6f7723e84300991
Marc Bachmann <marc.brookman@gmail.com>
2020-03-16 11:44:54 +0100

    fix: Remove the obsolete set-ci call in the entrypoint

--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-
 git fetch --tags -q
-
-read -r -d '' PATCHPACKAGE << EOL
-const pkg = require("./package.json");
-const scripts = pkg.scripts || {};
-scripts.preversion = undefined;
-scripts.postversion = "/release/node_modules/.bin/set-ci";
-pkg.scripts = scripts;
-fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 2));
-EOL
-
-node -e "$PATCHPACKAGE"
 
 if [ "$DRY_RUN" = "true" ]; then
   exec /release/node_modules/.bin/semantic-release --dry-run
